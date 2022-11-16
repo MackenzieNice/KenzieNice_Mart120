14b62527c7c26eeb7c67d1d079ef096ffcad45da
MackenzieNice/MART120 <112422279+MackenzieNice@users.noreply.github.com>
2022-11-15 18:28:15 -0700

    Add files via upload

---
+++ Homework 10/Sketch.js
@@ -0,0 +1,18 @@
+
+ function setup()
+{
+    createCanvas(500, 600);
+}
+
+ function draw()
+{
+    background(500,45,78);
+//
+fill(255,204,0)
+circle(50,100,75);
+
+//
+fill(0)
+circle(250,100,175)
+    
+}
